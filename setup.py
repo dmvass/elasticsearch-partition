281069ac591f9b6c377810bafe833fee55e02e15
Dmitry <vasilishin.d.o@gmail.com>
2019-02-21 22:57:30 +0200

    perf: Add Cython compilation and PyPy support

--- setup.py
+++ setup.py
@@ -1,8 +1,44 @@
-from os.path import join, dirname
+import glob
+from os import path
 import setuptools
 
+try:
+    from Cython.Distutils import build_ext
+    CYTHON = True
+except ImportError:
+    CYTHON = False
 
-with open(join(dirname(__file__), "README.md"), "r") as fh:
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+if CYTHON:
+    def modules(dirname):
+        filenames = glob.glob(path.join(dirname, "*.py"))
+
+        module_names = []
+        for name in filenames:
+            module, ext = path.splitext(path.basename(name))
+            if module != "__init__":
+                module_names.append(module)
+
+        return module_names
+
+    package_names = ["elasticsearch_partition"]
+    ext_modules = [
+        setuptools.Extension(
+            package + "." + module,
+            [path.join(*(package.split(".") + [module + ".py"]))]
+        )
+        for package in package_names
+        for module in modules(path.join(BASE_DIR, *package.split(".")))
+    ]
+
+    cmdclass = {'build_ext': build_ext}
+else:
+    ext_modules = []
+    cmdclass = {}
+
+
+with open(path.join(path.dirname(__file__), "README.md"), "r") as fh:
     long_description = fh.read()
 
 
@@ -13,12 +49,13 @@ setuptools.setup(
     author_email="vasilishin.d.o@gmail.com",
     description="A Python library for creating Elasticsearch partitioned indexes by date range",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/kandziu/elasticsearch-partition",
     packages=setuptools.find_packages(exclude=('test*')),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
@@ -29,5 +66,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     keywords=['elasticsearch', 'partition', 'partitioning'],
-    tests_require=['coverage']
+    tests_require=['coverage'],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules
 )
